92722150095e522525582f02aecb669a6cb4bd73
resufer <66846932+resufer@users.noreply.github.com>
2021-09-21 21:35:21 +0300

    Update settingsReducer.js

--- src/Redux/settingsReducer.js
+++ src/Redux/settingsReducer.js
@@ -1,3 +1,17 @@
-let settingsReducer;
+let initialState = {
+  fontSize: '14px',
+};
+
+function settingsReducer(state = initialState, action) {
+  switch (action.type) {
+    case "SET_SIZE":
+      return {
+        ...state,
+        fontSize: action.fontSize
+        }
+    default:
+      return state
+  }
+}
 
 export default settingsReducer;
