d8f1d508cc5d023b72b99c5f32f8d99b99c1ff65
resufer <66846932+resufer@users.noreply.github.com>
2021-09-21 21:36:10 +0300

    Update mainMenuReducer.js

--- src/Redux/mainMenuReducer.js
+++ src/Redux/mainMenuReducer.js
@@ -1,3 +1,17 @@
-let mainMenuReducer;
+let initialState = {
+  some: null,
+};
+
+function mainMenuReducer(state = initialState, action) {
+  switch (action.type) {
+    case "SET_SOME":
+      return {
+        ...state,
+        some: action.some
+        }
+    default:
+      return state
+  }
+}
 
 export default mainMenuReducer;
