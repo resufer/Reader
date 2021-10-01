0c1636fd664d9807091d62593782b6f75d628b15
resufer <66846932+resufer@users.noreply.github.com>
2021-10-01 19:52:11 +0300

    Update parserForNeuroBuddha.py

--- Python/parserForNeuroBuddha.py
+++ Python/parserForNeuroBuddha.py
@@ -1,1 +1,11 @@
+from bs4 import BeautifulSoup as Bs
+import requests
 
+page = 1
+book = ''
+
+while True:
+    r = requests.get('https://mir-knig.com/read_262117-' + str(page))
+    html = Bs(r.content, 'html.parser')
+
+    text = html.select('.text-block')
