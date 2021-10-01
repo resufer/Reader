7303eed6394fc1bfd98d83877920ee5beb3cd732
resufer <66846932+resufer@users.noreply.github.com>
2021-10-01 19:52:35 +0300

    Update parserForNeuroBuddha.py

--- Python/parserForNeuroBuddha.py
+++ Python/parserForNeuroBuddha.py
@@ -9,3 +9,12 @@ while True:
     html = Bs(r.content, 'html.parser')
 
     text = html.select('.text-block')
+
+    if len(text):
+        for p in text:
+            for txt in p.select('p'):
+                book += txt.text
+        print('current page is' + str(page))
+        page += 1
+    else:
+        break
